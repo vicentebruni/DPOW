f014d9413c57cb884bce021161e4f0daccc704b8
Vicente Bruni <51462000+vicentebruni@users.noreply.github.com>
2019-10-09 12:23:15 -0300

    09/10

--- Clase4/adivinarMatriz/public_html/js/funciones.js
+++ Clase4/adivinarMatriz/public_html/js/funciones.js
@@ -5,5 +5,5 @@ function borrar(){
 function generarNumero(){
     num=Math.round(9-1) * Math.random() + 1;
     num=Math.round(num);
-    alert(num);
+    return num;
 }
--- Clase4/adivinarnumero/public_html/js/funciones.js
+++ Clase4/adivinarnumero/public_html/js/funciones.js
@@ -1,14 +1,20 @@
 function adivinar(){
+    //Crear número aleatorio entre los valores recibidos del html
     var desde=document.getElementById("numminimo").value;
     var hasta=document.getElementById("nummax").value;
     num=Math.round(hasta-desde) * Math.random() + desde;
     num=Math.round(num);
-    var i = 0;
-    while(i<3){
-        var ing = parseInt(prompt("Ingrese numero entre " + desde + " y " + hasta + ": "));
+    //Verificar que los número coincidan
+    var i = 0; var intentosRestantes=3;
+    var ing = parseInt(prompt("Adivina el numero entre " + desde + " y " + hasta + ". Tienes 3 intentos!"));
+    while(i<2){
+        intentosRestantes--;
         if (ing === num){
             alert("Bien. El número era " + ing + ". ¡Lo adivinaste!");
             break;
+        }
+        else{
+            var ing = parseInt(prompt("No es " + ing + "\nTe quedan " + intentosRestantes + " intentos!\nAdivina el numero entre " + desde + " y " + hasta + "."));
         }
         i++;
     }
